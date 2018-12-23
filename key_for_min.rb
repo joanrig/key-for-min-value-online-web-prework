0a1add466c17020a9aacb56d24b67c45376542d0
Joan in DC <joanrigdon@gmail.com>
2018-12-23 05:40:18 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,18 @@
 
 def key_for_min_value(name_hash)
   prices = name_hash.collect do |name, price|
-  min_price = prices[0]
-  prices each.do |price|
-    if price < min_price
-  
+    min_price = prices[0]
+  end
+    i = 0
+    prices each.do |price|
+      if price < min_price
+        min_price = price
+        min_price_index = i
+      end
+      i += 1
     end
+    
+end
     
     
   
