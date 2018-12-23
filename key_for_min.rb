a26d1d649c8197e3f98304ce6160008341760dae
Joan in DC <joanrigdon@gmail.com>
2018-12-23 06:08:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,6 +3,7 @@
 
 def key_for_min_value(name_hash)
   values = name_hash.collect {|key, value| value}
+  keys = name_hash.collect {|key, value| key}
   
-  name_hash[price_list.find_index(min_price)]
+  
 end
