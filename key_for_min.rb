dbcb1b2f5e125e5bd82dbb5eef0737c02485829e
Joan in DC <joanrigdon@gmail.com>
2018-12-23 06:13:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -10,8 +10,9 @@ def key_for_min_value(name_hash)
   
   name_hash.do each |key, value|
     if value < low_value
-    low_value = value
-    low_key = key
+      low_value = value
+      low_key = key
+    end
   end
+  return low_key
 end
-  low_key
